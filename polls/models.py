405815b4213056c6c50d187368ef398c78215a4e
zeushqp <zeushqp@gmail.com>
2025-04-04 23:18:48 -0500

    segundo cambio de migraciones

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 class Question(models.Model):
     querstion_text = models.CharField(max_length=200)
@@ -6,11 +8,14 @@ class Question(models.Model):
 
     def __str__(self):
         return self.querstion_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-class Choise(models.Model):
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choise_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
-        return self.choise_text
+        return self.choice_text
